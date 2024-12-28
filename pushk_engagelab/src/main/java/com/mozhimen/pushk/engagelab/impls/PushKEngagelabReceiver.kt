d8f9a3ff3beedbed3ae3ac015c8d86a16be5c00b
mozhimen <58215586+mozhimen@users.noreply.github.com>
2024-12-28 20:48:27 +0800

    1228

--- pushk_engagelab/src/main/java/com/mozhimen/pushk/engagelab/impls/PushKEngagelabReceiver.kt
+++ pushk_engagelab/src/main/java/com/mozhimen/pushk/engagelab/impls/PushKEngagelabReceiver.kt
@@ -1,8 +1,7 @@
 package com.mozhimen.pushk.engagelab.impls
 
-import android.app.Activity
 import android.content.Context
-import android.content.Intent
+import androidx.annotation.CallSuper
 import com.engagelab.privates.common.component.MTCommonReceiver
 import com.engagelab.privates.core.api.MTCorePrivatesApi
 import com.engagelab.privates.core.api.WakeMessage
@@ -13,7 +12,6 @@ import com.engagelab.privates.push.api.MobileNumberMessage
 import com.engagelab.privates.push.api.NotificationMessage
 import com.engagelab.privates.push.api.PlatformTokenMessage
 import com.engagelab.privates.push.api.TagMessage
-import com.mozhimen.kotlin.utilk.android.util.UtilKLogWrapper
 import com.mozhimen.kotlin.utilk.commons.IUtilK
 import com.mozhimen.pushk.engagelab.PushKEngagelab
 
@@ -41,11 +39,12 @@ import com.mozhimen.pushk.engagelab.PushKEngagelab
  */
 open class PushKEngagelabReceiver : MTCommonReceiver(), IUtilK {
 
+    @CallSuper
     override fun onNotificationStatus(context: Context, enable: Boolean) {
         PushKEngagelab.instance.onNotificationStatus(context, enable)
     }
 
-
+    @CallSuper
     override fun onConnectStatus(context: Context, enable: Boolean) {
         // 长连接成功可获取registrationId
         if (enable) {
@@ -56,50 +55,62 @@ open class PushKEngagelabReceiver : MTCommonReceiver(), IUtilK {
         }
     }
 
+    @CallSuper
     override fun onNotificationArrived(context: Context, notificationMessage: NotificationMessage) {
         PushKEngagelab.instance.onNotificationArrived(context, notificationMessage)
     }
 
+    @CallSuper
     override fun onNotificationUnShow(context: Context, notificationMessage: NotificationMessage) {
         PushKEngagelab.instance.onNotificationUnShow(context, notificationMessage)
     }
 
+    @CallSuper
     override fun onNotificationClicked(context: Context, notificationMessage: NotificationMessage) {
         PushKEngagelab.instance.onNotificationClicked(context, notificationMessage)
     }
 
+    @CallSuper
     override fun onNotificationDeleted(context: Context, notificationMessage: NotificationMessage) {
         PushKEngagelab.instance.onNotificationDeleted(context, notificationMessage)
     }
 
+    @CallSuper
     override fun onCustomMessage(context: Context, customMessage: CustomMessage) {
         PushKEngagelab.instance.onCustomMessage(context, customMessage)
     }
 
+    @CallSuper
     override fun onTagMessage(context: Context, tagMessage: TagMessage) {
         PushKEngagelab.instance.onTagMessage(context, tagMessage)
     }
 
+    @CallSuper
     override fun onAliasMessage(context: Context, aliasMessage: AliasMessage) {
         PushKEngagelab.instance.onAliasMessage(context, aliasMessage)
     }
 
+    @CallSuper
     override fun onPlatformToken(context: Context, platformTokenMessage: PlatformTokenMessage) {
         PushKEngagelab.instance.onPlatformToken(context, platformTokenMessage)
     }
 
+    @CallSuper
     override fun onMobileNumber(context: Context, mobileNumberMessage: MobileNumberMessage) {
         PushKEngagelab.instance.onMobileNumber(context, mobileNumberMessage)
     }
 
+    @CallSuper
     override fun onWake(context: Context, wakeMessage: WakeMessage) {
         PushKEngagelab.instance.onWake(context, wakeMessage)
     }
 
+    @CallSuper
     override fun onInAppMessageShow(context: Context, inAppMessage: InAppMessage) {
         PushKEngagelab.instance.onInAppMessageShow(context, inAppMessage)
     }
 
+    @CallSuper
     override fun onInAppMessageClick(context: Context, inAppMessage: InAppMessage) {
         PushKEngagelab.instance.onInAppMessageClick(context, inAppMessage)
     }
